df18b1c71a8cc069f212cb309bd6e72ff20de2b0
Shri Krishna <skris56@uic.edu>
2024-11-14 20:54:54 -0600

    Update CORS origin to new deployment URL

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,7 @@ const mongoUrl =
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin: "https://playpal-hzkz.onrender.com",
+    origin: "https://playpal-ydmy.onrender.com",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
