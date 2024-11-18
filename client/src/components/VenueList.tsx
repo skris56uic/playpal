c4110c605cf9b47b6c101a6695b135f611b09704
Hrushikesh Joshi <hjosh@uic.edu>
2024-11-18 13:04:56 -0600

    changes from main

--- client/src/components/VenueList.tsx
+++ client/src/components/VenueList.tsx
@@ -1,117 +1,114 @@
 import { ReactNode, useEffect, useState } from "react";
 import {
-	Venue,
-	LocationDetails,
-	Country,
-	State,
-	City,
+  Venue,
+  LocationDetails,
+  Country,
+  State,
+  City,
 } from "../apis/interfaces";
 import { getVenueList } from "../apis";
 import ActionAreaCard from "./Card";
 import {
-	Divider,
-	FormControl,
-	Select,
-	Stack,
-	InputLabel,
-	MenuItem,
-	SelectChangeEvent,
-	Typography,
-	Box,
+  Divider,
+  FormControl,
+  Select,
+  Stack,
+  InputLabel,
+  MenuItem,
+  SelectChangeEvent,
+  Typography,
+  Box,
 } from "@mui/material";
 import Spinner from "./Spinner";
 import countriesJSON from "../countries_data/united_states.json";
 
 export function VenueList() {
-	const [venueList, setVenueList] = useState<Venue[]>([]);
-	const [location, setLocation] = useState<LocationDetails>({
-		latitude: 0,
-		longitude: 0,
-	});
-	const [loading, setLoading] = useState<boolean>(false);
-	const [selectedCountry, setSelectedCountry] = useState("");
-	const [selectedState, setSelectedState] = useState("");
-	const [selectedCity, setSelectedCity] = useState("");
-	const [states, setStates] = useState<State[]>([]);
-	const [cities, setCities] = useState<City[]>([]);
+  const [venueList, setVenueList] = useState<Venue[]>([]);
+  const [location, setLocation] = useState<LocationDetails>({
+    latitude: 0,
+    longitude: 0,
+  });
+  const [loading, setLoading] = useState<boolean>(false);
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [selectedState, setSelectedState] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+  const [states, setStates] = useState<State[]>([]);
+  const [cities, setCities] = useState<City[]>([]);
 
-	const handleCountryChange = (
-		event: SelectChangeEvent<string>,
-		_: ReactNode
-	) => {
-		const country = countriesJSON.find(
-			(c: Country) => c.name === event.target.value
-		);
-		setSelectedCountry(event.target.value as string);
-		setSelectedState("");
-		setSelectedCity("");
-		setStates(country ? country.states : []);
-		setCities([]);
-	};
+  const handleCountryChange = (
+    event: SelectChangeEvent<string>,
+    _: ReactNode
+  ) => {
+    const country = countriesJSON.find(
+      (c: Country) => c.name === event.target.value
+    );
+    setSelectedCountry(event.target.value as string);
+    setSelectedState("");
+    setSelectedCity("");
+    setStates(country ? country.states : []);
+    setCities([]);
+  };
 
-	const handleStateChange = (
-		event: SelectChangeEvent<string>,
-		_: ReactNode
-	) => {
-		const state = states.find((s: State) => s.name === event.target.value);
-		setSelectedState(event.target.value as string);
-		setSelectedCity("");
-		setCities(state ? state.cities : []);
-	};
+  const handleStateChange = (
+    event: SelectChangeEvent<string>,
+    _: ReactNode
+  ) => {
+    const state = states.find((s: State) => s.name === event.target.value);
+    setSelectedState(event.target.value as string);
+    setSelectedCity("");
+    setCities(state ? state.cities : []);
+  };
 
-	const handleCityChange = (
-		event: SelectChangeEvent<string>,
-		_: ReactNode
-	) => {
-		const city = cities.find((c: City) => c.name === event.target.value);
-		setSelectedCity(event.target.value as string);
-		if (city) {
-			setLocation({
-				latitude: parseFloat(city.latitude),
-				longitude: parseFloat(city.longitude),
-			});
-		}
-	};
+  const handleCityChange = (event: SelectChangeEvent<string>, _: ReactNode) => {
+    const city = cities.find((c: City) => c.name === event.target.value);
+    setSelectedCity(event.target.value as string);
+    if (city) {
+      setLocation({
+        latitude: parseFloat(city.latitude),
+        longitude: parseFloat(city.longitude),
+      });
+    }
+  };
 
-	useEffect(() => {
-		const fetchData = async () => {
-			if (location.latitude !== 0 && location.longitude !== 0) {
-				try {
-					setLoading(true);
-					const response = await getVenueList(location);
-					setVenueList(response);
-					setLoading(false);
-				} catch (error) {
-					console.error("Error fetching venue list:", error);
-					setLoading(false);
-				}
-			}
-		};
-		fetchData();
-	}, [location]);
+  useEffect(() => {
+    const fetchData = async () => {
+      if (location.latitude !== 0 && location.longitude !== 0) {
+        try {
+          setLoading(true);
+          const response = await getVenueList(location);
+          setVenueList(response);
+          setLoading(false);
+        } catch (error) {
+          console.error("Error fetching venue list:", error);
+          setLoading(false);
+        }
+      }
+    };
+    fetchData();
+  }, [location]);
 
-	if (loading) {
-		return (
-			<Spinner message="Loading Venues. Initially it might take upto 2 minutes. Please bear with us" />
-		);
-	}
+  if (loading) {
+    return (
+      <Spinner message="Loading Venues. Initially it might take upto 2 minutes. Please bear with us" />
+    );
+  }
 
-	return (
-		<Stack
-			direction="column"
-			divider={<Divider orientation="vertical" flexItem />}
-			spacing={2}
-		>
-			{venueList.map((x, i) => (
-				<ActionAreaCard
-					key={i}
-					id={x.id}
-					name={x.name}
-					location={x.location}
-					amenities={x.amenities.join(", ")}
-					price={x.availableSlots[0].timeSlots[0].price}
-				/>
-			))}
-		</Stack>
-	);
+  return (
+    <Stack
+      direction="column"
+      divider={<Divider orientation="vertical" flexItem />}
+      spacing={2}
+    >
+      {venueList.map((x, i) => (
+        <ActionAreaCard
+          key={i}
+          id={x.id}
+          name={x.name}
+          location={x.location}
+          amenities={x.amenities.join(", ")}
+          price={x.availableSlots[0].timeSlots[0].price}
+        />
+      ))}
+    </Stack>
+  );
 }
--- client/src/data/extract_data.mjs
+++ client/src/data/extract_data.mjs
@@ -7,42 +7,42 @@ const data = [];
 let country_object, state_object, city_object;
 
 for (let i = 0; i < json_data.length; i++) {
-	country_object = json_data[i];
-	console.log("country_number: ", i + 1);
-	console.log(country_object.name);
+  country_object = json_data[i];
+  console.log("country_number: ", i + 1);
+  console.log(country_object.name);
 
-	if (country_object.name === "United States") {
-		let states = [];
+  if (country_object.name === "United States") {
+    let states = [];
 
-		for (let j = 0; j < country_object.states.length; j++) {
-			state_object = country_object.states[j];
+    for (let j = 0; j < country_object.states.length; j++) {
+      state_object = country_object.states[j];
 
-			let cities = [];
+      let cities = [];
 
-			for (let k = 0; k < state_object.cities.length; k++) {
-				city_object = state_object.cities[k];
-				cities.push({
-					name: city_object.name,
-					latitude: city_object.latitude,
-					longitude: city_object.longitude,
-				});
-			}
+      for (let k = 0; k < state_object.cities.length; k++) {
+        city_object = state_object.cities[k];
+        cities.push({
+          name: city_object.name,
+          latitude: city_object.latitude,
+          longitude: city_object.longitude,
+        });
+      }
 
-			states.push({
-				name: state_object.name,
-				latitude: state_object.latitude,
-				longitude: state_object.longitude,
-				cities: cities,
-			});
-		}
+      states.push({
+        name: state_object.name,
+        latitude: state_object.latitude,
+        longitude: state_object.longitude,
+        cities: cities,
+      });
+    }
 
-		data.push({
-			name: country_object.name,
-			latitude: country_object.latitude,
-			longitude: country_object.longitude,
-			states: states,
-		});
-	}
+    data.push({
+      name: country_object.name,
+      latitude: country_object.latitude,
+      longitude: country_object.longitude,
+      states: states,
+    });
+  }
 }
 
 console.log(data.length);
